e8d74f36ce3f1c06b32bf730cfddcdaa48d9a846
Carl Fuller <carl@shelliraefarm.com>
2017-03-12 12:49:43 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Joe',
-		text: 'Sup yo?'
-	});
 });
 
 socket.on('disconnect', function () {
